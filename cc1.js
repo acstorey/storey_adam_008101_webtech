439f52fd74d1ef9c873701e92e807402b8eaac12
Adam <adamstorey@git.com>
2018-02-27 15:32:12 +0000

    First Commit

---
+++ cc1.js
@@ -0,0 +1,26 @@
+function encode(){ 
+
+    var original_text = document.getElementById("input").value; 
+    var coded_text = [];
+    var alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    
+    for(var idx =0; idx<original_text.length; idx++ ){
+        
+        
+        input = alphabet.indexOf(original_text[idx]); 
+        if(input == -1){
+            coded_text.push(original_text[idx]);
+            
+     
+        }
+        
+        else {
+            
+            var coded = (input+23)%26;
+            var letter = alphabet[coded]; 
+            coded_text.push(letter);
+        }
+    }
+document.getElementById("output").innerHTML = coded_text.join("");
+
+}
